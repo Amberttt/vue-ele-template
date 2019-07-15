a149a3f96938bfaad281f6f8d99db4f30d8ae348
WangJiaLiang <jialiang.wang@naxions.com>
2019-07-15 14:44:37 +0800

    eslint配置等

---
+++ src/components/main/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="main">
+        主页
+    </div>
+</template>
+<script>
+export default {
+    name: 'Main'
+    // data () {
+
+    // }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Main from '@/components/Main'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    routes: [
+        {
+            path: '/',
+            name: 'Main',
+            component: Main
+        },
+        {
+            path: '/login',
+            name: 'Login',
+            component: Login
+        }
+    ]
 })
